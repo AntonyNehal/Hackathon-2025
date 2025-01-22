eb0efed2c21556bc2af6b199a9c6df9195a7d12f
Antony Nehal <23102000oc@gmail.com>
2025-01-22 23:26:45 +0530

    installed react js and tailwind css

---
+++ hackathon/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "./Layout";
+import { Content } from "./Layout/Content";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+export default App;
